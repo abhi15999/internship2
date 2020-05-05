7eebc99677f74d88a2e8d29af63b36866ee07c61
Suman Rana <imranasuman@gmail.com>
2020-05-05 05:37:57 +0530

    last

--- server.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-let app = express();
-const PORT = process.env.PORT || 3000;
-app.use(express.static('src'));
-app.listen(PORT, function() {
-    console.log('Node app is running on post', PORT);
-});
